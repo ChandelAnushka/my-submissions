a1ba40c50f75327e6b25c1dccf2984847233b5f9
Anushka Chandel <51352909+ChandelAnushka@users.noreply.github.com>
2026-08-10 16:06:34 +0530

    Add: longest-consecutive-sequence - submission-9

---
+++ Data Structures & Algorithms/longest-consecutive-sequence/submission-9.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    int longestConsecutive(vector<int>& nums) {
+        if(nums.size()==0) return 0;
+        unordered_map<int, int> hashMap;
+        for(int i=0; i<nums.size(); i++) {
+            hashMap[nums[i]] = i;
+        }
+        vector<int> startSeq;
+        for(int i=0; i<nums.size(); i++) {
+            if(!(hashMap.contains(nums[i]-1))) {
+                startSeq.push_back(nums[i]);
+            }
+        }
+        cout<<endl;
+        int globalLargest=1, localLargest=1, number;
+        for(int i=0; i<nums.size(); i++) {
+        if(find(startSeq.begin(), startSeq.end(), nums[i]) != startSeq.end()) {
+            number = nums[i] + 1;
+            localLargest = 1;
+            while(true) {
+                if(hashMap.contains(number)) {
+                    localLargest++;
+                } else {
+                    break;
+                }
+                number++;
+            }
+        }
+        if (localLargest > globalLargest) globalLargest = localLargest;
+        }
+        return globalLargest;
+    }
+};
