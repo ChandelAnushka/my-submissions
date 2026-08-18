6d91011deede2b721d4b95376ecb62312046c067
Anushka Chandel <51352909+ChandelAnushka@users.noreply.github.com>
2026-08-18 22:31:36 +0530

    Add: populating-next-right-pointers-in-each-node - submission-1

---
+++ Data Structures & Algorithms/populating-next-right-pointers-in-each-node/submission-1.cpp
@@ -0,0 +1,47 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    Node* left;
+    Node* right;
+    Node* next;
+
+    Node() : val(0), left(NULL), right(NULL), next(NULL) {}
+
+    Node(int _val) : val(_val), left(NULL), right(NULL), next(NULL) {}
+
+    Node(int _val, Node* _left, Node* _right, Node* _next)
+        : val(_val), left(_left), right(_right), next(_next) {}
+};
+*/
+
+class Solution {
+   public:
+    Node* connect(Node* root) {
+        if (root == NULL) {
+            return root;
+        }
+        queue<Node*> q;
+        q.push(root);
+        while (!q.empty()) {
+            int i, num = q.size();
+            for (i = 0; i < num; i++) {
+                Node* curr = q.front();
+                q.pop();
+                if (i!=num-1) {
+                    if (q.front() != NULL) {
+                        curr->next = q.front();
+                    }
+                }
+                if (curr->left != NULL) {
+                    q.push(curr->left);
+                }
+                if (curr->right != NULL) {
+                    q.push(curr->right);
+                }
+            }
+        }
+        return root;
+    }
+};
